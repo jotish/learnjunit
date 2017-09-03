0c36fe2f695ce07703db9d388bdf995de0c260d7
jotish <jotish@ymail.com>
2017-09-03 15:30:38 +0530

    fixed untracked files

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -1,16 +1,33 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by jotishsuthar on 03/09/17.
  */
+@RunWith(JUnitParamsRunner.class)
 public class MoneyTest {
 
+    private static final Object[] getMoney() {
+        return new Object[] {
+                new Object[] {10, "USD"},
+                new Object[] {20, "EUR"}
+        };
+    }
+
     @Test
-    public void constructorShouldSetAmountAndCurrency(){
-        Money money = new Money(10, "USD");
-        assertEquals(10, money.getAmount());
-        assertEquals("USD", money.getCurrency());
+    @Parameters(method = "getMoney")
+    public void constructorShouldSetAmountAndCurrency(
+            int amount, String currency) {
+        Money money = new Money(amount, currency);
+        assertEquals(amount, money.getAmount());
+        assertEquals(currency, money.getCurrency());
     }
 }
